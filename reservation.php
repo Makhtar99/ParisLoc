5a28a87e318c6f7dd6463d179c29b940c7814359
Makhtar99 <127096437+Makhtar99@users.noreply.github.com>
2023-06-16 13:55:32 +0200

    working

--- reservation.php
+++ reservation.php
@@ -1,66 +1,96 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Réservation de logement</title>
+    <title>Détails et réservation du logement</title>
 </head>
 <body>
-    <h1>Réservation de logement</h1>
-    
-    <form method="POST" action="reservation.php">
-        <label for="Date_depart">Date d'arrivée :</label>
-        <input type="date" id="Date_depart" name="Date_depart" required><br>
-        
-        <label for="Date_arrivée">Date de fin :</label>
-        <input type="date" id="Date_arrivée" name="Date_arrivée" required><br>
-        
-        
-        <label for="Nombre_personnes">Nombre de personnes :</label>
-        <input type="number" id="Nombre_personnes" name="Nombre_personnes" required><br>
-        
-        <input type="submit" value="Réserver">
-    </form>
+    <h1>Détails et réservation du logement</h1>
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "Airbnb";
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "Airbnb";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Échec de la connexion à la base de données : " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Échec de la connexion à la base de données : " . $conn->connect_error);
+    }
 
-session_start();
+    session_start();
 
-if (!isset($_SESSION['user'])) {
-    die("Veuillez vous connecter pour effectuer une réservation.");
-}
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (!isset($_SESSION['user'])) {
+            die("Veuillez vous connecter pour effectuer une réservation.");
+        }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $dateDépart = $_POST['Date_depart'];
-    $dateFin = $_POST['Date_arrivée'];
-    $nombrePersonnes = $_POST['Nombre_personnes'];
-    $utilisateurConnecte = $_SESSION['user']['id'];
+        $dateDépart = $_POST['Date_depart'];
+        $dateFin = $_POST['Date_arrivée'];
+        $nombrePersonnes = $_POST['Nombre_personnes'];
+        $utilisateurConnecte = $_SESSION['user']['id'];
+        $logementId = $_GET['id'];
 
+        // Vérification des disponibilités
+        $dispoSql = "SELECT * FROM Hébergements WHERE ID = '$logementId' AND Date_depart <= '$dateDépart' AND Date_arrivée >= '$dateFin'";
+        $dispoResult = $conn->query($dispoSql);
 
-    $sql = "INSERT INTO Réservations ( ID_utilisateur, Date_depart, Date_arrivée, Nombre_personnes)
-            VALUES ( '$utilisateurConnecte', '$dateDépart', '$dateFin', '$nombrePersonnes')";
+        if ($dispoResult->num_rows > 0) {
+            $sql = "INSERT INTO Réservations (ID_utilisateur, Date_depart, Date_arrivée, Nombre_personnes, ID_hébergement)
+                VALUES ('$utilisateurConnecte', '$dateDépart', '$dateFin', '$nombrePersonnes', '$logementId')";
+
             if ($conn->query($sql) === TRUE) {
                 echo "Réservation effectuée avec succès.";
             } else {
                 echo "Erreur lors de la réservation : " . $conn->error;
             }
-            
-}
+        } else {
+            echo "Le logement n'est pas disponible aux dates sélectionnées.";
+        }
+    }
 
+    if (isset($_GET['id'])) {
+        $logementId = $_GET['id'];
 
-$conn->close();
-?>
+        $sql = "SELECT * FROM Hébergements WHERE ID = '$logementId'";
+        $result = $conn->query($sql);
 
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            echo "<h2>Nom : " . $row["Titre"] . "</h2>";
+            echo "<img src='" . $row["Image"] . "' alt='Image du logement'><br>";  
+            echo "<p>Description : " . $row["Description"] . "</p>";
+            echo "<p>Commodités : " . $row["Commodités"] . "</p>";
+            echo "<p>Règles de la maison : " . $row["Règles_de_la_maison"] . "</p>";
+            echo "<p>Début : " . $row["Date_depart"] . "</p>";
+            echo "<p>Fin : " . $row["Date_arrivée"] . "</p>";
+            echo "<p>Arrondissement : " . $row["localisation"] . "</p>";
+            echo "<p>Places : " . $row["capacite"] . "</p>";
 
+            echo "<form method='POST' action='?id=" . $row["ID"] . "'>";
+            echo "<label for='Date_depart'>Date d'arrivée :</label>";
+            echo "<input type='date' id='Date_depart' name='Date_depart' required><br>";
+            echo "<label for='Date_arrivée'>Date de fin :</label>";
+            echo "<input type='date' id='Date_arrivée' name='Date_arrivée' required><br>";
+            echo "<label for='Nombre_personnes'>Nombre de personnes :</label>";
+            echo "<input type='number' id='Nombre_personnes' name='Nombre_personnes' required><br>";
+            echo "<input type='submit' value='Réserver'>";
+            echo "</form>";
+        } else {
+            echo "Logement introuvable.";
+        }
+    } else {
+        echo "Aucun logement sélectionné.";
+    }
+
+    $conn->close();
+    ?>
 </body>
 </html>
+
+
+
+
+
 
 
--- welcome.php
+++ welcome.php
@@ -1,47 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Liste des logements</title>
+</head>
+<body>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "Airbnb";
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "Airbnb";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Erreur de connexion à la base de données : " . $conn->connect_error);
-}
-
-$localisation = isset($_POST['localisation']) ? $_POST['localisation'] : "";
-$capacite = isset($_POST['capacite']) ? $_POST['capacite'] : "";
-$dateDebut = isset($_POST['date_debut']) ? $_POST['date_debut'] : "";
-$dateFin = isset($_POST['date_fin']) ? $_POST['date_fin'] : "";
-
-if (empty($localisation) && empty($capacite) && empty($dateDebut) && empty($dateFin)) {
-    $sql = "SELECT * FROM Hébergements LIMIT 6";
-} else {
-    $sql = "SELECT * FROM Hébergements WHERE localisation LIKE '%$localisation%' AND capacite >= '$capacite' AND Date_depart >= '$dateDebut' AND Date_arrivée <= '$dateFin'";
-}
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "<div>";
-        echo "<h2>Nom : " . $row["Titre"] . "</h2>";
-        echo "<p>Début : " . $row["Date_depart"] . "</p>";
-        echo "<p>Fin : " . $row["Date_arrivée"] . "</p>";
-        echo "<p>Arrondissement : " . $row["localisation"] . "</p>";
-        echo "<p>Places : " . $row["capacite"] . "</p>";
-        echo "</div>";
-        echo "<br>";
+    if ($conn->connect_error) {
+        die("Erreur de connexion à la base de données : " . $conn->connect_error);
     }
-} else {
-    $approxSql = "SELECT * FROM Hébergements ORDER BY RAND() LIMIT 6";
-    $approxResult = $conn->query($approxSql);
 
-    if ($approxResult->num_rows > 0) {
-        while ($row = $approxResult->fetch_assoc()) {
+    $localisation = isset($_POST['localisation']) ? $_POST['localisation'] : "";
+    $capacite = isset($_POST['capacite']) ? $_POST['capacite'] : "";
+    $dateDebut = isset($_POST['date_debut']) ? $_POST['date_debut'] : "";
+    $dateFin = isset($_POST['date_fin']) ? $_POST['date_fin'] : "";
+
+    if (empty($localisation) && empty($capacite) && empty($dateDebut) && empty($dateFin)) {
+        $sql = "SELECT * FROM Hébergements LIMIT 6";
+    } else {
+        $sql = "SELECT * FROM Hébergements WHERE localisation LIKE '%$localisation%' AND capacite >= '$capacite' AND Date_depart >= '$dateDebut' AND Date_arrivée <= '$dateFin'";
+    }
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
             echo "<div>";
-            echo "<h2>Nom : " . $row["Titre"] . "</h2>";
+            echo "<h2><a href='reservation.php?id=" . $row["ID"] . "'>" . $row["Titre"] . "</a></h2>";
             echo "<p>Début : " . $row["Date_depart"] . "</p>";
             echo "<p>Fin : " . $row["Date_arrivée"] . "</p>";
             echo "<p>Arrondissement : " . $row["localisation"] . "</p>";
@@ -50,20 +41,31 @@ if ($result->num_rows > 0) {
             echo "<br>";
         }
     } else {
-        echo "Aucun hébergement trouvé dans la base de données.";
+        $approxSql = "SELECT * FROM Hébergements ORDER BY RAND() LIMIT 6";
+        $approxResult = $conn->query($approxSql);
+
+        if ($approxResult->num_rows > 0) {
+            while ($row = $approxResult->fetch_assoc()) {
+                echo "<div>";
+                echo "<h2><a href='reservation.php?id=" . $row["ID"] . "'>" . $row["Titre"] . "</a></h2>";
+                echo "<p>Début : " . $row["Date_depart"] . "</p>";
+                echo "<p>Fin : " . $row["Date_arrivée"] . "</p>";
+                echo "<p>Arrondissement : " . $row["localisation"] . "</p>";
+                echo "<p>Places : " . $row["capacite"] . "</p>";
+                echo "</div>";
+                echo "<br>";
+            }
+        } else {
+            echo "Aucun hébergement trouvé dans la base de données.";
+        }
     }
-}
 
-$conn->close();
+    $conn->close();
 ?>
+</body>
+</html>
 
-<form method="post" action="">
-    <input type="text" name="localisation" placeholder="Arrondissement">
-    <input type="number" name="capacite" placeholder="Places">
-    <input type="date" name="date_debut" placeholder="Date de début">
-    <input type="date" name="date_fin" placeholder="Date de fin">
-    <input type="submit" value="Rechercher">
-</form>
+
 
 
 
