d3b962f3236ae8d87af292bb875bd24f48a35bed
Makhtar99 <127096437+Makhtar99@users.noreply.github.com>
2023-06-17 16:28:40 +0200

    working

--- welcome.php
+++ welcome.php
@@ -4,6 +4,13 @@
     <title>Liste des logements</title>
 </head>
 <body>
+<form method="post" action="">
+    <input type="text" name="localisation" placeholder="Arrondissement" value="<?php echo isset($_POST['localisation']) ? $_POST['localisation'] : ''; ?>">
+    <input type="number" name="capacite" placeholder="Places" value="<?php echo isset($_POST['capacite']) ? $_POST['capacite'] : ''; ?>">
+    <input type="date" name="date_debut" placeholder="Date de début" value="<?php echo isset($_POST['date_debut']) ? $_POST['date_debut'] : ''; ?>">
+    <input type="date" name="date_fin" placeholder="Date de fin" value="<?php echo isset($_POST['date_fin']) ? $_POST['date_fin'] : ''; ?>">
+    <input type="submit" value="Rechercher">
+</form>
 <?php
     $servername = "localhost";
     $username = "root";
@@ -22,7 +29,7 @@
     $dateFin = isset($_POST['date_fin']) ? $_POST['date_fin'] : "";
 
     if (empty($localisation) && empty($capacite) && empty($dateDebut) && empty($dateFin)) {
-        $sql = "SELECT * FROM Hébergements LIMIT 6";
+        $sql = "SELECT * FROM Hébergements LIMIT 7";
     } else {
         $sql = "SELECT * FROM Hébergements WHERE localisation LIKE '%$localisation%' AND capacite >= '$capacite' AND Date_depart >= '$dateDebut' AND Date_arrivée <= '$dateFin'";
     }
@@ -41,7 +48,7 @@
             echo "<br>";
         }
     } else {
-        $approxSql = "SELECT * FROM Hébergements ORDER BY RAND() LIMIT 6";
+        $approxSql = "SELECT * FROM Hébergements ORDER BY RAND() LIMIT 7";
         $approxResult = $conn->query($approxSql);
 
         if ($approxResult->num_rows > 0) {
@@ -64,6 +71,8 @@
 ?>
 </body>
 </html>
+
+
 
 
 
