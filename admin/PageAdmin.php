24075ef26f0fcbc3351f1a3909dbb819af58beef
AGBLEY béni <agbleybeni@gmail.com>
2023-06-21 19:54:25 +0200

    creation de la page admin

---
+++ admin/PageAdmin.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="PageAdmin.css">
+    <title>Admin Page </title>
+    <link rel="icon" type="image/png" href="components/logo2.png">
+
+</head>
+<body >
+        <?php include"components/Header.php"?>
+        <div class="wrapper">
+        <section class="list_img">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="https://prod-saint-gobain-fr.content.saint-gobain.io/sites/saint-gobain.fr/files/2020-06/amenagement-interieur-conseils-et-solutions-00.jpg" alt="img_logement" class="img_logement" >
+                        
+                            <h2>Nom du logement</h2>
+                        
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <img src="https://www.architecte-maisons.fr/wp-content/uploads/2019/02/agencement-pieces.jpg" alt="img_logement" class="img_logement" >
+                            
+                            <h2>Nom du logement</h2>
+                           
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <img src="https://www.architecte-maisons.fr/wp-content/uploads/2019/02/agencement-pieces.jpg" alt="img_logement" class="img_logement" >
+                            
+                            <h2>Nom du logement</h2>
+                           
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.architecte-maisons.fr/wp-content/uploads/2019/02/agencement-pieces.jpg" alt="img_logement" class="img_logement" >
+                            
+                            <h2>Nom du logement</h2>
+                           
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://prod-saint-gobain-fr.content.saint-gobain.io/sites/saint-gobain.fr/files/2020-06/amenagement-interieur-conseils-et-solutions-00.jpg" alt="img_logement" class="img_logement" >
+                            
+                            <h2>Nom du logement</h2>
+                           
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://prod-saint-gobain-fr.content.saint-gobain.io/sites/saint-gobain.fr/files/2020-06/amenagement-interieur-conseils-et-solutions-00.jpg" alt="img_logement" class="img_logement" >
+                            
+                            <h2>Nom du logement</h2>
+                           
+                        </a>
+                    </li>
+                    
+                </ul>
+
+        </section>
+    </div>  
+        <?php include"components/Footer.php"?>
+</body>  
+
+</html>
+
+
+
